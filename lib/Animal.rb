4ab62c919455cfd2ca419e9c7857c91f9e656031
Mark DeNardo <mark.denardo@gmail.com>
2019-08-29 12:53:49 -0400

    solution

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,29 @@
 class Animal
+    attr_reader :species, :weight, :name, :zoo
+
+    @@all = []
+
+    def initialize(species, weight, name)
+        @species=species
+        @weight=weight
+        @name=name
+        @@all<<self
+    end
+
+    def zoo=(zoo)
+        @zoo=zoo
+        zoo.animals<<self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        @@all.select do |animal|
+            animal.species == species
+        end
+
+    end
 
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,45 @@
 class Zoo
 
+    attr_reader :name, :location, :animals
+
+    @@all=[]
+
+    def initialize(name, location)
+        @name=name
+        @location=location
+        @animals = []
+        @@all<<self
+    end
+
+    def animal_species
+        nonuniq = @animals.each.collect do |animal|
+            animal.species
+        end
+        nonuniq.uniq
+    end
+
+    def find_by_species(species)
+        @animals.select do |animal| 
+        animal.species == species
+        end
+
+    end
+
+    def animal_nicknames
+        @animals.each.collect do |animal|
+        animal.name
+        end
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_location(location)
+        @@all.select do |zoo| 
+            zoo.location == location
+        end
+    end
+
 end
