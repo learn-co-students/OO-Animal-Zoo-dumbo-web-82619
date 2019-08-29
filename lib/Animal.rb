1d636930ed89bb6d907cca21706cd73f4d2dd11a
Neal <no4259@gmail.com>
2019-08-29 18:28:49 -0400

    Finished

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,24 @@
 class Animal
+    attr_reader :species, :nickname, :weight, :zoo
+    @@all = []
 
+    def initialize(species, nickname, weight, zoo)
+        @species = species
+        @nickname = nickname
+        @weight = weight
+        @zoo = zoo
+        @@all << self
+    end
+
+
+    def Animal.all
+        return @@all
+    end
+
+    def self.find_by_species(species)
+        arr = Animal.all.select do |animal|
+            animal.species == species
+        end
+        arr
+    end
 end
