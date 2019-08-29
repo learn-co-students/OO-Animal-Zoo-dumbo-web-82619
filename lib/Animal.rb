88f70fb93dc789413b9e4ab032f488f01fe888d4
github username <github email address>
2019-08-29 12:46:38 -0400

    First commit

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,25 @@
 class Animal
 
+    attr_reader :nickaname, :species, :zoo
+    attr_accessor :weight
+
+    @@animal_array = []
+
+    def initialize (nickname, species, weight, zoo)
+
+        @nickname = nickname
+        @species = species
+        @weight = weight
+        @zoo = zoo
+        @@animal_array << self
+
+    end
+
+    def self.all
+        return @@animal_array
+    end
+
+
+
+
 end
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,55 @@
 class Zoo
 
+attr_reader :name, :location
+
+@@zoos = []
+
+def initialize(name, location)
+
+    @name = name
+    @location = location
+    @@zoos << self
+
+end
+
+def self.all
+    return @@zoos
+end
+
+def animals
+
+    Animal.all.select do |animal|
+        animal.zoo == self
+    end
+end
+
+def animal_species
+
+    species_array = []
+
+    animals_in_zoo = self.animals
+
+    animals_in_zoo.each do |animal|
+
+        species_array << animal.species
+
+    end
+
+
+    species_array.uniq
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 end
