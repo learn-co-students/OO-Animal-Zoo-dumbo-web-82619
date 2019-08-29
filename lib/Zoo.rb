577254a42def083976f43157288a3745b9928c68
github username <github email address>
2019-08-29 15:21:13 -0400

    done

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,54 @@
 class Zoo
+    attr_reader(:name, :location)
+    @@all = []
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
 
+    def Zoo.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+    end
+
+    def animal_species
+        species = self.animals.map do |animal|
+            animal.species
+        end
+        species.uniq
+    end
+    
+    def find_by_species(species)
+        all_species = self.animals.select do |animal|
+            animal.species.downcase == species.downcase
+        end
+        if all_species.size == 0
+            "Sorry, we do not have that species here"
+        else
+            all_species
+        end
+    end
+
+    def animal_nicknames
+        self.animals.map do |animal|
+            animal.nickname
+        end
+    end
+
+    def Zoo.find_by_location(location)
+        zoo_locations = Zoo.all.select do |zoo|
+            zoo.location.downcase == location.downcase
+        end
+        if zoo_locations.size == 0
+            "We do not have a zoo at that location"
+        else
+            zoo_locations
+        end
+    end
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,18 @@ require 'pry'
 
 
 #Test your code here
+bronx = Zoo.new("Bronx Zoo", "Bronx Borough")
+manhattan = Zoo.new("Manhattan Zoo", "NYC")
+brooklyn = Zoo.new("Brooklyn Zoo", "Brooklyn Borough")
+brooklyn2 = Zoo.new("Other Brooklyn Zoo", "Brooklyn Borough")
+
+tiger = Animal.new("Tiger", 153, "Leo", bronx)
+lion = Animal.new("Lion", 164, "Tigger", manhattan)
+hawk = Animal.new("Hawk", 24, "Hawkeye", brooklyn)
+seal = Animal.new("Seal", 104, "Dewgong", brooklyn)
+leopard = Animal.new("Leopard", 155, "Panther", brooklyn)
+elephant = Animal.new("Elephant", 600, "Dumbo", brooklyn)
+elephant2 = Animal.new("Elephant", 700, "Cetha", brooklyn)
 
 
 binding.pry
