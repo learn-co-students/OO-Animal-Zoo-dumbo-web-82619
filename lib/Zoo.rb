292e84f71a68d5e4c6be3acd8c6fc3d1052bb0a9
lsamano <leizl.samano@flatironschool.com>
2019-08-29 12:04:54 -0400

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,40 @@
 class Zoo
+    attr_reader :name, :location, :animals
+    @@all = []
 
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+        @animals = []
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animal_species
+        self.animals.map do |animal| 
+            animal.species
+        end
+        .uniq!
+    end
+
+    def animal_nicknames
+        self.animals.map do |animal| 
+            animal.nickname
+        end
+    end
+
+    def find_by_species(species)
+        self.animals.filter do |animal|
+            animal.species == species
+        end
+    end
+
+    def self.find_by_location(location)
+        self.all.filter do |zoo|
+            zoo.location == location
+        end
+    end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,15 @@ require 'pry'
 
 
 #Test your code here
+bk_zoo = Zoo.new("The Zoo", "Brooklyn")
+anim1 = Animal.new("dog", 22, "doggie", bk_zoo)
+anim2 = Animal.new("cat", 23, "kitty", bk_zoo)
+anim3 = Animal.new("dog", 55, "frost", bk_zoo)
 
+fe_zoo = Zoo.new("Garreg Mach", "Fodlan")
+anim1 = Animal.new("deer", 22, "Claude", fe_zoo)
+anim2 = Animal.new("eagle", 23, "Edelgard", fe_zoo)
+anim3 = Animal.new("lion", 55, "Dimitri", fe_zoo)
 
 binding.pry
 puts "done"
